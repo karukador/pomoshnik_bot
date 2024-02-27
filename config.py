553d56bb38dafcece9034ae6437b09519e371de3
karukador <152086077+karukador@users.noreply.github.com>
2024-02-27 14:59:37 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-my_TOKEN = "7054419137:AAEVaTPSC4YMcFa95WKYcNaRCq3Uz640b4g"
-
-GPT_LOCAL_URL = 'http://localhost:1234/v1/chat/completions'
-GPT_WEB_URL = 'обратиться к наставнику'
-
-HEADERS = {"Content-Type": "application/json"}
-
-MAX_TOKENS = 150
